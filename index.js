467932adce3d33506d44bcffbac669351c207c69
weemeng <weemeng@users.noreply.github.com>
2020-01-08 12:35:23 +0800

    adding cowsaysmoo

---
+++ index.js
@@ -0,0 +1,7 @@
+const cows = require ("cows")(); //calling the cows function with the ()
+const index = process.argv[2] || 0;
+console.log(cows[index]);
+
+Math.random * 5;
+
+//console.log(Math.max(...cows.map(el=>el.length)));
